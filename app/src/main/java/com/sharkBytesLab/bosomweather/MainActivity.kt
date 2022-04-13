3dc784c8a3f524c698d975128446569b90eefb7a
ShashankMangal <shashankmangal10@gmail.com>
2022-04-13 14:02:07 +0530

    minor commit

--- app/src/main/java/com/sharkBytesLab/bosomweather/MainActivity.kt
+++ app/src/main/java/com/sharkBytesLab/bosomweather/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = ""
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
